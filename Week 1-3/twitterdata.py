abeb1f3e9e5b65a03447ec203fae896c7a1e225f
RanksDG <danagreene29@gmail.com>
2019-07-22 15:19:53 -0400

    Using a link for styling options.

--- Week 1-3/twitterdata.py
+++ Week 1-3/twitterdata.py
@@ -15,12 +15,13 @@ wordcloud = WordCloud().generate(text)
 
 # Next we want to open the JSON file. We tag this file as
 # "r" read only because we are only going to look at the data.
-tweetFile = open("../Desktop/tweets_small.json", "r")
+tweetFile = open("twitterdata.json", "r")
 
 # We use the JSON library to get data from the file as JSON data.
 tweetData = json.load(tweetFile)
-print(len(tweetData))
-print(list(tweetData[0].keys()))
+tweetFile.close()
+# print(len(tweetData))
+# print(list(tweetData[0].keys()))
 #['created_at', 'favorite_count', 'hashtags', 'id', 'id_str', 'lang', 'retweet_count', 'source', 'text', 'truncated', 'urls', 'user', 'user_mentions']
 favorite_count = 0
 
@@ -37,16 +38,16 @@ for t in range(0,len(tweetData)):
 		#tweets += tweetData[t]["text"]
 		tweets.append(tweetData[t]["text"])
 #print(tweets)
-print(tweets[0])
+# print(tweets[0])
 Avg = favorite_count/len(tweetData)
-print(Avg)
+# print(Avg)
 
 polaritylist = []
 for item in tweets:
 	blob1 = TextBlob(item)
 	polar1 = blob1.polarity
 	polaritylist.append(polar1)
-print(polaritylist)
+# print(polaritylist)
 
 
 
@@ -64,25 +65,69 @@ for i in range (0,len(tweetData)):
 	text.append(tweetData[i]['text'])
 	polarity_list.append(TextBlob(tweetData[i]['text']).polarity)
 
+# print(polarity_list)
+# print(min(polarity_list))
+# print(max(polarity_list))
+#n,bins,patches = plt.hist(polarity_list)
+# plt.axis([-0.55,1.05,0,50])
+
 #giant_string = ""
 
 tweetstring = ""
 for tweet in tweets:
 	tweet +=tweet + " "
 	tweetstring += tweet
-print(tweetstring)
+# print(tweetstring)
 
-wordcloud = WordCloud().generate(tweetstring)
-plt.imshow(wordcloud, interpolation = "bilinear")
-plt.axis("off")
-plt.show()
-plt.savefig("alischart.png")
+wordcloud = WordCloud(height = 1000, width = 1000).generate(tweetstring)
+# plt.figure(figsize = (10, 10), facecolor = None)
+# plt.imshow(wordcloud, interpolation = "bilinear")
+# plt.axis("off")
+# plt.show()
+#plt.savefig("alischart.png")
 
 a_counter = 0
 b_counter = 0
 
+
+
+def countletter(string,letter):
+	counter = 0
+	for let in string:
+		if let.lower()== letter:
+			counter += 1
+	return counter
+
+# alefbet = sorted('qwertyuiopasdfghjklzxcvbnm')
+# occurences = []
+# for i in alefbet:
+# 	occurences.append(countletter(tweetstring,i))
+# 	print(f'letter: {i} occurences: {countletter(tweetstring, i)}')
+# print(occurences)
 # We can close the file now that we have locally stored the data.
-tweetFile.close()
+def wordcount(tweetString,string1):
+	counter = 0
+	string1 = string1.lower()
+	wordList = tweetString.split(" ")
+	for i in wordList:
+		if i == string1:
+			counter += 1
+	return counter
+
+wordCountList = []
+for item in tweets:
+	wordoccurence = wordcount(item,"the")
+	wordCountList.append(wordoccurence)
+print(wordCountList)
+print(len(wordCountList))
+
+print(wordCountList)
+print(min(wordCountList))
+print(max(wordCountList))
+n,bins,patches = plt.hist(wordCountList)
+plt.axis([-1,4,0,90])
+plt.grid(True)
+plt.show()
 
 #tb = TextBlob("You are a brilliant computer scientist.")
 # print(tb.polarity)
@@ -93,7 +138,16 @@ tweetFile.close()
 
 #plt.bar([1,3,5,7,9],[5,2,7,8,2],)
 
+# n, bins, patches = plt.hist(polarity_list, 50, normed=1, facecolor='g', alpha=0.75)
 
+
+# plt.xlabel('Smarts')
+# plt.ylabel('Probability')
+# plt.title('Histogram of IQ')
+# plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+# plt.axis([40, 160, 0, 0.03])
+# plt.grid(True)
+# plt.show()
 
 
 # # We then print the data of ONE tweet:
